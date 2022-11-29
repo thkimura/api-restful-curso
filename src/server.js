f7f5c732e956e80b6521be622077ea0ea485cad2
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2022-11-29 19:04:42 -0300

    estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+//const path = require('path')
+
+//const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+// Conexao com banco de dados
+//db.connect()
+
+//habilitar server para receber dados via post (formulario)
+app.use(express.urlencoded({ extended: true}))
+
+//definindo as rotas
+app.use('/api', routes)
+
+// executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
