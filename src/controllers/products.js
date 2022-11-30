d77b21fcdb42cb4e0ddd8ecfad1e38f85c2dc115
Thalles Hideki Kimura <thalles.kimura@gmail.com>
2022-11-29 23:58:22 -0300

    adicionando metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -10,7 +10,6 @@ async function get (req, res) {
   res.send (products)
 }
 
-
 async function post (req, res) {
   const {
     name,
@@ -32,7 +31,7 @@ async function post (req, res) {
 }
 
 async function put (req, res) {
-  const { id }= req.params
+  const { id } = req.params
 
   const product = await ProductsModel.findOneAndUpdate({ _id: id }, req.body, { new: true })
 
@@ -42,8 +41,23 @@ async function put (req, res) {
   })
 }
 
+async function remove (req, res) {
+
+  const { id } = req.params
+  
+  const remove = await ProductsModel.deleteOne({ _id:id })
+
+  const message = remove ? 'success' : 'error'
+
+  res.send({
+    message,
+  })
+  
+}
+
 module.exports = {
   get,
   post,
   put,
+  remove,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ const ProductsController = require('../controllers/products')
 router.get('/products/:id?', ProductsController.get)
 router.post('/products', ProductsController.post)
 router.put('/products/:id', ProductsController.put)
-//router.delete('/products', ProductsController.delete)
+router.delete('/products/:id', ProductsController.remove)
 
 
 module.exports = router
